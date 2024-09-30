46b2dd3481691eb34b6e8fe291fea4f8154c9b7f
gevorg-moviex <gevorg.sahakyan.ait@gmail.com>
2024-09-30 21:59:51 +0400

    Changes deploying

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import Home from './Pages/home'
 import Contact from './Pages/contact';
-import { Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
   )
 }
