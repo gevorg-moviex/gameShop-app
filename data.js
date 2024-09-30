eb604f88dab305ee2f009869c3511e35cb2c7054
gevorg-moviex <gevorg.sahakyan.ait@gmail.com>
2024-09-30 22:54:14 +0400

    Adding images

--- data.js
+++ data.js
@@ -1,28 +1,28 @@
 export const products1 = [
     {
         id: 1,
-        imageUrl: "/assets/Products_1/product1-1.webp",
+        imageUrl: "https://static.wixstatic.com/media/c837a6_9a2583f0a17e48efb075913d90da0dbf~mv2.jpg/v1/fill/w_375,h_375,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_9a2583f0a17e48efb075913d90da0dbf~mv2.jpg",
         title: "Echo Headset",
         price: "$ 39.99",
         count: 1
     },
     {
         id: 2,
-        imageUrl: "/assets/Products_1/product1-2.webp",
+        imageUrl: "https://static.wixstatic.com/media/c837a6_85410586580e46ce94659e34046a48c8~mv2.jpg/v1/fill/w_375,h_375,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_85410586580e46ce94659e34046a48c8~mv2.jpg",
         title: "Mach Gaming Chair",
         price: "$ 116.99",
         count: 1
     },
     {
         id: 3,
-        imageUrl: "/assets/Products_1/product1-3.webp",
+        imageUrl: "https://static.wixstatic.com/media/c837a6_0067919b3086427cad03a54e61452958~mv2.jpg/v1/fill/w_375,h_375,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_0067919b3086427cad03a54e61452958~mv2.jpg",
         title: "X-2 Wireless Mouse",
         price: "$ 24.99",
         count: 1
     },
     {
         id: 4,
-        imageUrl: "/assets/Products_1/product1-4.webp",
+        imageUrl: "https://static.wixstatic.com/media/c837a6_7c11d47692f04c1f92a024ad095274ae~mv2.jpg/v1/fill/w_375,h_375,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c837a6_7c11d47692f04c1f92a024ad095274ae~mv2.jpg",
         title: "Spartan Mechanical Keyboard",
         price: "$ 44.49",
         count: 1
@@ -137,17 +137,17 @@ export const portfolio = [
     {
         id: 19,
         title: "Consoles",
-        imageUrl: "/assets/Portfolio/portfolio-1.webp"
+        imageUrl: "https://static.wixstatic.com/media/c837a6_dbd7e89c57bf4488bd0b2ae612679ac4~mv2.jpg/v1/fill/w_212,h_283,q_90/c837a6_dbd7e89c57bf4488bd0b2ae612679ac4~mv2.webp"
     },
     {
         id: 20,
         title: "Accesories",
-        imageUrl: "/assets/Portfolio/portfolio-2.webp"
+        imageUrl: "https://static.wixstatic.com/media/c837a6_86d07bbcc9b04e268cb6af68cb5c9e10~mv2.jpg/v1/fill/w_212,h_283,q_90/c837a6_86d07bbcc9b04e268cb6af68cb5c9e10~mv2.webp"
     },
     {
         id: 21,
         title: "Controllers",
-        imageUrl: "/assets/Portfolio/portfolio-3.webp"
+        imageUrl: "https://static.wixstatic.com/media/c837a6_870af96f67744ed8b5eb7c239430c5c7~mv2.jpg/v1/fill/w_212,h_283,fp_0.75_0.36,q_90/c837a6_870af96f67744ed8b5eb7c239430c5c7~mv2.webp"
     },
 ]
 
