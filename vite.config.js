59d5ca1192f7e59e3aae69159ea29236b0ccd9ad
gevorg-moviex <gevorg.sahakyan.ait@gmail.com>
2024-09-30 18:16:34 +0400

    Again changes github errors

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/gameShop-app/', 
+  base: '/gameShop-app/dist/', 
   plugins: [react()],
 })
