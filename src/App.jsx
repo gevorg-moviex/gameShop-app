0c8cc6df6665dd0927848a0890ea780803d19efa
gevorg-moviex <gevorg.sahakyan.ait@gmail.com>
2024-09-30 17:58:01 +0400

    Removing /home

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
